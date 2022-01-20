a32740d1d5f8841d0d7386adf3bdc8ac7679aac8
stock2342 <jromeroor@unal.edu.co>
2022-01-20 15:41:09 -0500

    the method's veryfyEmail and verifyPassword were set

--- src/data/User.java
+++ src/data/User.java
@@ -1,5 +1,8 @@
 package data;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class User {
     protected String name;
     protected String numberPhone;
@@ -46,11 +49,42 @@ public class User {
     }
 
     public boolean verifyEmail(String email){
-        return false;
-        //TODO
+        String regex = "^[\\\\w!#$%&’*+/=?`{|}~^-]+(?:\\\\.[\\\\w!#$%&’*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\\\.)+[a-zA-Z]{2,6}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
     }
     public boolean verifyPassword(String password){
-        return false;
-        //TODO
+        if(password.length() < 8) return false;
+
+        boolean upperCase = false;
+        boolean lowerCase = false;
+        boolean digits = false;
+        boolean symbols = false;
+
+        for(int i = 0; i<password.length(); i++){
+            char car = password.charAt(i);
+            if(Character.isWhitespace(car))
+                return false;
+            if(Character.isUpperCase(car))
+                upperCase = true;
+            if(Character.isLowerCase(car))
+                lowerCase = true;
+            if(Character.isDigit(car))
+                digits = true;
+            if(!Character.isDigit(car) && !Character.isLetter(car) && !Character.isWhitespace(car))
+                symbols = true;
+        }
+        int conditions = 0;
+        if(upperCase)
+            conditions++;
+        if(lowerCase)
+            conditions++;
+        if(digits)
+            conditions++;
+        if(symbols)
+            conditions++;
+
+        return conditions >= 3;
     }
 }
