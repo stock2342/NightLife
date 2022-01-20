818abecd49f34fb4b9508dff32617d156188eb5e
stock2342 <jromeroor@unal.edu.co>
2022-01-20 14:19:16 -0500

    Subida inicial, creación del paquete data y la superclase User (First commit, creation of the "data" package and the superclass "User")

---
+++ src/data/User.java
@@ -0,0 +1,54 @@
+package data;
+
+public class User {
+    protected String name;
+    protected String numberPhone;
+    protected String email;
+    protected String password;
+
+    public User(String name, String numberPhone, String email, String password) {
+        this.name = name;
+        this.numberPhone = numberPhone;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumberPhone() {
+        return numberPhone;
+    }
+
+    public void setNumberPhone(String numberPhone) {
+        this.numberPhone = numberPhone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean verifyEmail(String email){
+        return false;
+    }
+    public boolean verifyPassword(String password){
+        return false;
+    }
+}
